01d4dc35a07312314dc7d974ad8834a16c0489e8
Kevin Sandoval <ksandovaldev@gmail.com>
2021-03-30 11:09:20 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://ksandoval:caracara@cluster0.g0kql.mongodb.net/barista?retryWrites=true&w=majority', 
+    'dbName': 'barista'
+};
